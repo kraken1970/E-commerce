0f45116e8f547732a8819b09e1a1c4a95fe1c6da
Serg Minin <everestdon@ukr.net>
2018-05-23 19:14:17 +0300

    block 5, banner + js ok

--- src/assets/JS/app.js
+++ src/assets/JS/app.js
@@ -1,1 +1,27 @@
-console.log('TEST');
+$(function() {
+
+    $("#banner_wrap").slick({
+        lazyLoad: 'ondemand',
+        infinite: true,
+        dots: true,
+        autoplay: false, //поменять на true
+        autoplaySpeed: 3000,
+        arrows: false
+    });
+});
+
+const buttonMinus = document.querySelectorAll('.number-minus');
+const buttonPlus = document.querySelectorAll('.number-plus');
+let count = document.querySelectorAll('.number-count');
+
+for (let i = 0; i < count.length; i++) {
+    buttonMinus[i].onclick = function() {
+        if (count[i].value > 1) {
+            count[i].value -= 1;
+        }
+    }
+
+    buttonPlus[i].onclick = function() {
+        count[i].value = Number(count[i].value) + 1;
+    }
+}
